a36c1789ef2e132a833c57c866ff97b69316cd4b
Yoonjae,Cho <yoonjae.cho92@gmail.com>
2016-05-11 17:32:40 +0000

    Get result from server

--- src/client.c
+++ src/client.c
@@ -11,76 +11,107 @@
 
 int main(int argc, char** argv)
 {
-	int fd,fd2;
-	int i;
+    int fd,fd2;
+    int i;
 
-	int* ptr;
-	int* ptr2;
-	int flag = PROT_WRITE | PROT_READ;
+    int* ptr;
+    int* ptr2;
+    int flag = PROT_WRITE | PROT_READ;
 
-	sem_t* startSem = sem_open("startSem", 0);
-	sem_t* clientNumSem = sem_open("clientNumSem", 0);
-	sem_t* test = sem_open("test", 0);
+    sem_t* startSem = sem_open("startSem", 0);
+    sem_t* clientNumSem = sem_open("clientNumSem", 0);
+    sem_t* inputNumSem = sem_open("inputNumSem", 0);
+    sem_t* waitClientSem = sem_open("waitClientSem", 0);
+    sem_t* waitServerPrintSem = sem_open("waitServerPrintSem", 0);
 
-	int semVal;
+    int semVal;
 
-	int userNum;
+    int userNum;
+
+    int winCheck;
 
 
-	if((fd = open("input.txt", O_RDWR, 0666)) < 0) {
-	    perror("File open error");
-	    exit(1);
+    if((fd = open("input.txt", O_RDWR, 0666)) < 0) {
+	perror("File open error");
+	exit(1);
+    }
+
+    if((ptr = (int *)mmap(0, 4096, flag, MAP_SHARED, fd ,0)) == NULL) {
+	perror("mmap error");
+	exit(1);
+    }
+
+    if((fd2 = open("output.txt", O_RDWR, 0666)) < 0) {
+	perror("File open error");
+	exit(1);
+    }
+
+    if((ptr2 = (int *)mmap(0, 4096, flag, MAP_SHARED, fd2 ,0)) == NULL) {
+	perror("mmap error");
+	exit(1);
+    }
+
+/*
+    sem_getvalue(startSem, &semVal);
+    printf("startSem : %d\n",semVal);
+
+
+    sem_getvalue(clientNumSem, &semVal);
+    printf("clientNumSem : %d\n",semVal);
+    */
+
+    if(!semVal) {
+	printf("You can't enter this room! Two clients already using.\n");
+	exit(1);
+    }
+
+    sem_wait(clientNumSem);
+/*
+    sem_getvalue(clientNumSem, &semVal);
+    printf("clientNumSem : %d\n",semVal);
+*/
+    /* Starting game */
+
+    printf("======================\n");
+    printf("     Hello [%d]\n",getpid());
+    printf("     Game Start!!\n");
+    printf("======================\n");
+
+    sem_post(startSem);
+
+    winCheck = 0;
+
+    while(1) {
+
+	//sem_wait(inputNumSem);
+	sem_post(waitClientSem);
+
+	printf("Input Number(100 - 999) : ");
+	scanf("%d",&userNum);
+	ptr[0] = userNum;
+
+	sem_wait(waitServerPrintSem);
+	printf("stirke : %d, ball : %d\n",ptr2[0], ptr2[1]);
+
+	if(ptr2[0] == 3) {
+	    printf("[%d] %d : %d strike, %d ball\n",getpid(), userNum, ptr2[0], ptr2[1]);
+	    printf("You Win\n");
+	    winCheck++;
+	} else {
+	    printf("[%d] %d : %d strike, %d ball\n",getpid(), userNum, ptr2[0], ptr2[1]);
 	}
-
-	if((ptr = (int *)mmap(0, 4096, flag, MAP_SHARED, fd ,0)) == NULL) {
-	    perror("mmap error");
-	    exit(1);
-	}
-
-	if((fd2 = open("output.txt", O_RDWR, 0666)) < 0) {
-	    perror("File open error");
-	    exit(1);
-	}
-
-	if((ptr2 = (int *)mmap(0, 4096, flag, MAP_SHARED, fd2 ,0)) == NULL) {
-	    perror("mmap error");
-	    exit(1);
-	}
-
-	sem_getvalue(test, &semVal);
-	printf("test: %d\n",semVal);
-
-	sem_getvalue(startSem, &semVal);
-	printf("startSem : %d\n",semVal);
-
 	
-	sem_getvalue(clientNumSem, &semVal);
-	printf("clientNumSem : %d\n",semVal);
+	//sem_post(inputNumSem);
+	if(winCheck) {
 
-	if(!semVal) {
-	    printf("You can't enter this room! Two clients already using.\n");
-	    exit(1);
 	}
+    }
 
-	sem_wait(clientNumSem);
-	
-	sem_getvalue(clientNumSem, &semVal);
-	printf("clientNumSem : %d\n",semVal);
+    getchar();
 
-	/* Starting game */
 
-	printf("======================\n");
-	printf("     Hello [%d]\n",getpid());
-	printf("     Game Start!!\n");
-	printf("======================\n");
+    close(fd);
+    close(fd2);
 
-	sem_post(startSem);
-
-	getchar();
-
-	
-	close(fd);
-	close(fd2);
-
-	sem_post(clientNumSem);
+    sem_post(clientNumSem);
 }
--- src/server.c
+++ src/server.c
@@ -11,90 +11,174 @@
 #include <time.h>
 
 int randomGenerator() {
-    	int ranNum;
-   	srand(time(NULL));
-	ranNum = (rand() % 900) + 100;
-	return ranNum;
+    int ranNum;
+    srand(time(NULL));
+    ranNum = (rand() % 900) + 100;
+    return ranNum;
 }
 
 int main(int argc, char** argv)
 {
-	int fd,fd2;
-	int i;
+    int fd,fd2;
+    int i,j;
 
-	int* ptr;
-	int* ptr2;
+    int* ptr;
+    int* ptr2;
 
-	sem_t* startSem;
-	sem_t* clientNumSem;
-	sem_t* test;
+    sem_t* startSem;
+    sem_t* clientNumSem;
+    sem_t* inputNumSem;
+    sem_t* waitClientSem;
+    sem_t* waitServerPrintSem;
 
-	int semVal;
+    int semVal;
 
-	int zero = 0;
+    int zero = 0;
+    int temp;
 
-	int ranNum;
+    int ranNum;
+    int random[3];
 
-	fd = open("input.txt", O_RDWR|O_CREAT, S_IRWXU);
-	write(fd, &zero, sizeof(int));
-	ptr = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	close(fd);
-	
-	fd2 = open("output.txt", O_RDWR|O_CREAT, S_IRWXU);
-	write(fd2, &zero, sizeof(int));
-	ptr2 = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd2, 0);
-	close(fd2);
+    int userNum;
+    int user[3];
 
-	sem_unlink("startSem");
-	if((startSem = sem_open("startSem", O_CREAT, 0644, 0)) == SEM_FAILED) {
-	    perror("Sem Failed");
-	    exit(1);
+    int check[3];
+
+    int strike;
+    int ball;
+
+    fd = open("input.txt", O_RDWR|O_CREAT, S_IRWXU);
+    write(fd, &zero, sizeof(int));
+    ptr = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    close(fd);
+
+    fd2 = open("output.txt", O_RDWR|O_CREAT, S_IRWXU);
+    write(fd2, &zero, sizeof(int));
+    ptr2 = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd2, 0);
+    close(fd2);
+
+    sem_unlink("startSem");
+    if((startSem = sem_open("startSem", O_CREAT, 0644, 0)) == SEM_FAILED) {
+	perror("Sem Failed");
+	exit(1);
+    }
+
+    sem_unlink("clientNumSem");
+    if((clientNumSem = sem_open("clientNumSem", O_CREAT, 0644, 2)) == SEM_FAILED) {
+	perror("Sem Failed");
+	exit(1);
+    }
+
+    sem_unlink("inputNumSem");
+    if((inputNumSem = sem_open("inputNumSem", O_CREAT, 0644, 1)) == SEM_FAILED) {
+	perror("Sem Failed");
+	exit(1);
+    }
+
+    sem_unlink("waitClientSem");
+    if((waitClientSem = sem_open("waitClientSem", O_CREAT, 0644, 0)) == SEM_FAILED) {
+	perror("Sem Failed");
+	exit(1);
+    }
+
+    sem_unlink("waitServerPrintSem");
+    if((waitServerPrintSem = sem_open("waitServerPrintSem", O_CREAT, 0644, 0)) == SEM_FAILED) {
+	perror("Sem Failed");
+	exit(1);
+    }
+
+/*
+    sem_getvalue(startSem, &semVal);
+    printf("startSem : %d\n",semVal);
+    */
+
+    ranNum = randomGenerator();
+    temp = ranNum;
+    i=2;
+    while(ranNum > 0) {
+	random[i--] = ranNum % 10;
+	ranNum /= 10;
+    }
+    ranNum = temp;
+    printf("Random Number : ");
+    for(i=0; i<3; i++) {
+	printf("%d",random[i]);
+    }
+    puts("");
+
+    
+    sem_wait(startSem);
+    printf("Client hi\n");
+
+
+    /*
+    sem_getvalue(startSem, &semVal);
+    printf("startSem : %d\n",semVal);
+    */
+
+    sem_wait(startSem);
+
+    printf("Client hi\n");
+    /*
+    sem_getvalue(startSem, &semVal);
+    printf("startSem : %d\n",semVal);
+    */
+
+    /* Starting game */
+    printf("=========================\n");
+    printf("        Game Start       \n");
+    printf("=========================\n");
+
+
+    while(1) {
+	sem_wait(waitClientSem);
+
+	printf("userNum : %d\n",ptr[0]);
+	printf("ranNum : %d\n",ranNum);
+	userNum = ptr[0];
+	temp = userNum;
+	i=2;
+	while(userNum > 0) {
+	    user[i--] = userNum % 10;
+	    userNum /= 10;
+	}
+	userNum = temp;
+	strike = 0;
+	ball = 0;
+	memset(check,0,sizeof(check));
+
+	for(i=0; i<3; i++) {
+	    if(random[i] == user[i]) {
+		strike++;
+		check[i]++;
+	    }
 	}
 
-	sem_unlink("clientNumSem");
-	if((clientNumSem = sem_open("clientNumSem", O_CREAT, 0644, 2)) == SEM_FAILED) {
-	    perror("Sem Failed");
-	    exit(1);
+	if(strike == 3) {
+	    ptr2[0] = 3;
+	    ptr2[1] = 0;
+	} else {
+	    for(i=0; i<3; i++) {
+		if(check[i]) continue;
+		for(j=0; j<3; j++) {
+		    if(check[j]) continue;
+		    if(user[i] == random[j]) {
+			ball++;
+			break;
+		    }
+		}
+	    }
+	    ptr2[0] = strike;
+	    ptr2[1] = ball;
 	}
-	
-	sem_unlink("test");
-	if((test = sem_open("test", O_CREAT, 0644, 0)) == SEM_FAILED) {
-	    perror("Sem Failed");
-	    exit(1);
-	}
+	sem_post(waitServerPrintSem);
+
+    }
+
+    getchar();
 
 
 
-	sem_getvalue(startSem, &semVal);
-	printf("startSem : %d\n",semVal);
-
-	ranNum = randomGenerator();
-
-	printf("Random number : %d\n",ranNum);
-	
-	sem_wait(startSem);
-	printf("Client hi\n");
-	
-	sem_getvalue(startSem, &semVal);
-	printf("startSem : %d\n",semVal);
-	sem_wait(startSem);
-	
-	printf("Client hi\n");
-	sem_getvalue(startSem, &semVal);
-	printf("startSem : %d\n",semVal);
-
-	printf("=========================\n");
-	printf("        Game Start       \n");
-	printf("=========================\n");
-
-	getchar();
-	
-	if((startSem = sem_open("startSem", O_CREAT, 0644, -100)) == SEM_FAILED) {
-	    perror("Sem Failed");
-	    exit(1);
-	}
-
-
-	sem_close(startSem);
-	exit(0);
+    sem_close(startSem);
+    exit(0);
 }
